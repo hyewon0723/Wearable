d31a493e817e08e97a80903cd6eeb1fd41be87c9
Luke Kim <hyewon0723@gmail.com>
2016-11-14 18:24:46 -0800

    first commit

---
+++ app/src/main/java/com/example/android/sunshine/app/sync/DataLayerListenerService.java
@@ -0,0 +1,42 @@
+package com.example.android.sunshine.app.sync;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class DataLayerListenerService extends WearableListenerService {
+    GoogleApiClient mGoogleApiClient;
+    private static final String requestDataPath = "/wear-request-path";
+    private static final String LOG_TAG = "Luke";
+
+    public DataLayerListenerService() {
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+        Log.v(LOG_TAG, "Received a message");
+
+        if(messageEvent.getPath().equals(requestDataPath)) {
+            Log.v(LOG_TAG, "we received a message from the wearable to fetch data and send it over");
+
+            Context context = DataLayerListenerService.this.getApplicationContext();
+            SunshineSyncAdapter.syncImmediately(context);
+        }
+    }
+}
